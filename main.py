9661058e5bf0e56160ddaeeeebd7a98f6b863feb
root <root@vmi1685062.contaboserver.net>
2025-02-13 18:59:38 +0100

    Second simulation of CD bot testing

--- main.py
+++ main.py
@@ -17,9 +17,7 @@ app.add_middleware(
 app.include_router(api_router, prefix=settings.API_PREFIX)
 
 
-@app.get("/stage2")
-async def stage2():
-    return {"message": "Welcome to Stage 2"}
+
 @app.get("/healthcheck")
 async def health_check():
     """Checks if server is active."""
